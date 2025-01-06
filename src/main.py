26d8835bf53a0640bf13bca9a5b86a0efa14125f
Ivan Rassadin <ir@lmx.su>
2025-01-06 08:59:42 +0400

    Fixed typos

--- src/main.py
+++ src/main.py
@@ -12,19 +12,19 @@ from trades.trade_specific_market import create_and_submit_order
 
 load_dotenv()
 
-# Step 1: generate new wallet and save PBK with PK to .env
+# Step 1: Generate a new wallet and save the PBK with PK to .env
 if os.getenv('PK') is None:
-  generate_new_wallet()
+    generate_new_wallet()
 
-# # Step 2: send some matic to generated wallet to update allowances
-# set_allowances()
+# Step 2: Send some MATIC to the generated wallet to update allowances
+set_allowances()
 
-# # Step 3: generate API credentials, so that we can communicate with polymarket
-# generate_api_keys()
+# Step 3: Generate API credentials so that we can communicate with Polymarket
+generate_api_keys()
 
-# Step 4: find condition id for market you want to trade and retrive market info from CLOB
+# Step 4: Find the condition ID for the market you want to trade and retrieve market info from CLOB
 market = get_market('0xbb96f092cb5d54138c6af2ae824bb276c3e20969fb2acfced30ac7f88f60862e')
 yes_token = next((item for item in market['tokens'] if item.get('outcome') == 'Yes'), None)
 
-# Step 5: fill order data and choose side you want to buy
+# Step 5: Fill order data and choose the side you want to buy
 create_and_submit_order(yes_token['token_id'], BUY, 0.044, 26)
